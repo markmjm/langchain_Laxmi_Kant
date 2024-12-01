3db4a0edf6076e52124e58e64911da85e73b4061
Mark <33471734+markmjm@users.noreply.github.com>
2024-11-30 19:11:08 -0500

    project updates

---
+++ 05. Output Parsing/output_parsing_datetime.py
@@ -0,0 +1,45 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import ChatOllama
+from langchain.output_parsers import DatetimeOutputParser
+
+load_dotenv()
+LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2")
+LANGCHAIN_ENDPOINT = os.getenv("LANGCHAIN_ENDPOINT")
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
+LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT")
+
+# https://python.langchain.com/api_reference/ollama/chat_models/langchain_ollama.chat_models.ChatOllama.html#langchain_ollama.chat_models.ChatOllama
+# https://python.langchain.com/api_reference/core/prompts/langchain_core.prompts.chat.ChatPromptTemplate.html
+
+base_url = "http://localhost:11434"
+model = 'llama3.2:latest'
+
+llm = ChatOllama(
+    base_url=base_url,
+    model=model,
+    #temperature=0.8,
+    num_predict=256
+)
+# print(llm.invoke("Tell me a joke about Washington DC"))
+##
+parser = DatetimeOutputParser()
+#print(parser.get_format_instructions())
+joke_prompt = PromptTemplate(
+    template='''
+    Answer the user query with a datetime only.   Here is your formating instructions.
+    {format_instructions}
+
+    Query: {query}
+    Answer:''',
+    input_variables=['query'],
+    partial_variables={'format_instructions': parser.get_format_instructions()}
+)
+myChain = joke_prompt | llm | parser
+output = myChain.invoke({'query': 'When America was discovered?'})
+print(output)
+
+
+
---
+++ 06. Chat Message Memory/message_memory.py
@@ -0,0 +1,66 @@
+import os
+from dotenv import load_dotenv
+from langchain_core.prompts import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder
+)
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories import SQLChatMessageHistory
+from langchain_ollama import ChatOllama
+
+load_dotenv()
+LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2")
+LANGCHAIN_ENDPOINT = os.getenv("LANGCHAIN_ENDPOINT")
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
+LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT")
+
+# https://python.langchain.com/api_reference/ollama/chat_models/langchain_ollama.chat_models.ChatOllama.html#langchain_ollama.chat_models.ChatOllama
+# https://python.langchain.com/api_reference/core/prompts/langchain_core.prompts.chat.ChatPromptTemplate.html
+
+base_url = "http://localhost:11434"
+model = 'llama3.2:latest'
+
+llm = ChatOllama(
+    base_url=base_url,
+    model=model,
+    # temperature=0.8,
+    num_predict=256
+)
+
+
+def get_session_history(session_id):
+    return SQLChatMessageHistory(session_id, "sqlite:///chat_history.db")
+
+
+system = SystemMessagePromptTemplate.from_template("You are helpful assistant.")
+human = HumanMessagePromptTemplate.from_template("{input}")
+
+messages = [system, MessagesPlaceholder(variable_name='history'), human]
+
+prompt = ChatPromptTemplate(messages=messages)
+
+chain = prompt | llm | StrOutputParser()
+
+runnable_with_history = RunnableWithMessageHistory(chain, get_session_history,
+                                                   input_messages_key='input',
+                                                   history_messages_key='history')
+
+
+def chat_with_llm(session_id, input):
+    output = runnable_with_history.invoke(
+        {'input': input},
+        config={'configurable': {'session_id': session_id}}
+    )
+
+    return output
+
+
+user_id = "Santa Clause"
+about = "My name is Santa Clause. I am the CEO for Santa Clause Enterprises."
+print(chat_with_llm(user_id, about))
+about = "what is my name?"
+print(chat_with_llm(user_id, about))
